f31883e6a9fc90f789c426e2ade55d27823cfc0b
karenzhao35 <karenzhao35@gmail.com>
2024-05-27 19:26:36 -0400

    edited carousel

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 ddev start
 # Update Tailwind CSS
 npx tailwindcss -i ./web/css/input.css -o ./web/css/output.css --watch
-# Install Glide.js
-ddev exec npm install @glidejs/glide
--- web/index.js
+++
@@ -1,9 +0,0 @@
-import Glide from "https://cdn.jsdelivr.net/npm/@glidejs/glide@3.4.1/dist/glide.esm.js";
-
-const glide = new Glide(".glide", {
-    type: "carousel",
-    focusAt: "center",
-    perView: 3
-});
-
-glide.mount();
