f166a67d50ab7c229a23f63738f1a38a4a5979c4
karenzhao35 <karenzhao35@gmail.com>
2024-12-19 19:33:06 -0800

    add dependencies for build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  build: {
+    target: 'esnext', 
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
